1778495352e3a629968399645d88d689a1ce039d
Ahmed Moussa <moussa.ahmed95@gmail.com>
2019-04-11 19:54:50 +0200

    Changed The Design

--- Hashing.swift
+++ Hashing.swift
@@ -5,131 +5,194 @@
 import Foundation
 import CommonCrypto
 
-public class Hashing {
+public protocol Hash {
+    @discardableResult
+    func normalHash(_ data: UnsafeRawPointer!, _ len: CC_LONG, _ md: UnsafeMutablePointer<UInt8>!) -> UnsafeMutablePointer<UInt8>!
     
-    public enum HashingError: Error {
-        case HMACHashNoKey
+    func hmacHash(_ algorithm: CCHmacAlgorithm, _ key: UnsafeRawPointer!, _ keyLength: Int, _ data: UnsafeRawPointer!, _ dataLength: Int, _ macOut: UnsafeMutableRawPointer!)
+    
+    func hash(string: String) -> String
+}
+
+public class baseHash: Hash {
+    
+    var digestLength: Int {
+        return 0
     }
     
-    public enum HashingAlgorithm {
-        case HMACMD5, HMACSHA1, HMACSHA224, HMACSHA256, HMACSHA384, HMACSHA512, MD5, SHA1, SHA224, SHA256, SHA384, SHA512
-        
-        var HMACAlgorithm: CCHmacAlgorithm {
-            var result: Int = 0
-            switch self {
-            case .HMACMD5:
-                result = kCCHmacAlgMD5
-                break
-            case .HMACSHA1:
-                result = kCCHmacAlgSHA1
-                break
-            case .HMACSHA224:
-                result = kCCHmacAlgSHA224
-                break
-            case .HMACSHA256:
-                result = kCCHmacAlgSHA256
-                break
-            case .HMACSHA384:
-                result = kCCHmacAlgSHA384
-                break
-            case .HMACSHA512:
-                result = kCCHmacAlgSHA512
-                break
-            default:
-                break
-            }
-            return CCHmacAlgorithm(result)
-        }
-        
-        var digestLength: Int {
-            var result: Int32 = 0
-            switch self {
-            case .MD5, .HMACMD5:
-                result = CC_MD5_DIGEST_LENGTH
-                break
-            case .SHA1, .HMACSHA1:
-                result = CC_SHA1_DIGEST_LENGTH
-                break
-            case .SHA224, .HMACSHA224:
-                result = CC_SHA224_DIGEST_LENGTH
-                break
-            case .SHA256, .HMACSHA256:
-                result = CC_SHA256_DIGEST_LENGTH
-                break
-            case .SHA384, .HMACSHA384:
-                result = CC_SHA384_DIGEST_LENGTH
-                break
-            case .SHA512, .HMACSHA512:
-                result = CC_SHA512_DIGEST_LENGTH
-                break
-            }
-            return Int(result)
-        }
+    var HMACAlgorithm: CCHmacAlgorithm {
+        return CCHmacAlgorithm(0)
     }
     
-    public class func hash(string: String, hashType: HashingAlgorithm, key: String?) throws -> String {
-        
-        func hexStringFromData(input: NSData) -> String {
-            var bytes = [UInt8](repeating: 0, count: input.length)
-            input.getBytes(&bytes, length: input.length)
-            var hexString = ""
-            for byte in bytes {
-                hexString += String(format:"%02x", UInt8(byte))
-            }
-            return hexString
-        }
-        
-        func hexStringFromData(input: UnsafeMutablePointer<CUnsignedChar>, length: Int) -> String {
-            let hash = NSMutableString()
-            for i in 0..<length {
-                hash.appendFormat("%02x", input[i])
-            }
-            return String(hash)
-        }
-        
-        @discardableResult
-        func normalHash(hashType: HashingAlgorithm, _ data: UnsafeRawPointer!, _ len: CC_LONG, _ md: UnsafeMutablePointer<UInt8>!) -> UnsafeMutablePointer<UInt8>! {
-            switch hashType {
-            case .MD5, .HMACMD5:
-                return CC_MD5(data, len, md)
-            case .SHA1, .HMACSHA1:
-                return CC_SHA1(data, len, md)
-            case .SHA224, .HMACSHA224:
-                return CC_SHA224(data, len, md)
-            case .SHA256, .HMACSHA256:
-                return CC_SHA256(data, len, md)
-            case .SHA384, .HMACSHA384:
-                return CC_SHA384(data, len, md)
-            case .SHA512, .HMACSHA512:
-                return CC_SHA512(data, len, md)
-            }
-        }
-        
-        switch hashType {
-        case .MD5, .SHA1, .SHA224, .SHA256, .SHA384, .SHA512:
+    private let key: String?
+    
+    init(key: String? = nil) {
+        self.key = key
+    }
+    
+    public func hash(string: String) -> String {
+        if (self.key == nil) {
             if let stringData = string.data(using: String.Encoding.utf8) {
                 let stringNSData = stringData as NSData
-                let digestLength = hashType.digestLength
-                var hash = [UInt8](repeating: 0, count: digestLength)
-                normalHash(hashType: hashType, stringNSData.bytes, UInt32(stringNSData.length), &hash)
-                let digest = NSData(bytes: hash, length: digestLength)
-                return hexStringFromData(input: digest)
+                var hash = [UInt8](repeating: 0, count: self.digestLength)
+                self.normalHash(stringNSData.bytes, UInt32(stringNSData.length), &hash)
+                let digest = NSData(bytes: hash, length: self.digestLength)
+                return self.hexStringFromData(input: digest)
             }
             return ""
-        case .HMACMD5, .HMACSHA1, .HMACSHA224, .HMACSHA256, .HMACSHA384, .HMACSHA512:
-            if (key == nil) {
-                throw HashingError.HMACHashNoKey
-            }
+        } else {
             let str = string.cString(using: String.Encoding.utf8)
             let strLen = Int(string.lengthOfBytes(using: String.Encoding.utf8))
-            let digestLen = hashType.digestLength
-            let result = UnsafeMutablePointer<CUnsignedChar>.allocate(capacity: digestLen)
-            let keyStr = key!.cString(using: String.Encoding.utf8)
-            let keyLen = Int(key!.lengthOfBytes(using: String.Encoding.utf8))
-            CCHmac(hashType.HMACAlgorithm, keyStr!, keyLen, str!, strLen, result)
-            let digest = hexStringFromData(input: result, length: digestLen)
+            let result = UnsafeMutablePointer<CUnsignedChar>.allocate(capacity: self.digestLength)
+            let keyStr = self.key!.cString(using: String.Encoding.utf8)
+            let keyLen = Int(self.key!.lengthOfBytes(using: String.Encoding.utf8))
+            self.hmacHash(self.HMACAlgorithm, keyStr!, keyLen, str!, strLen, result)
+            let digest = hexStringFromData(input: result, length: self.digestLength)
             result.deallocate()
             return digest
         }
     }
+    
+    @discardableResult
+    public func normalHash(_ data: UnsafeRawPointer!, _ len: CC_LONG, _ md: UnsafeMutablePointer<UInt8>!) -> UnsafeMutablePointer<UInt8>! {
+        fatalError("Not implemented")
+    }
+    
+    public func hmacHash(_ algorithm: CCHmacAlgorithm, _ key: UnsafeRawPointer!, _ keyLength: Int, _ data: UnsafeRawPointer!, _ dataLength: Int, _ macOut: UnsafeMutableRawPointer!) {
+        fatalError("Not implemented")
+    }
+    
+    func hexStringFromData(input: NSData) -> String {
+        var bytes = [UInt8](repeating: 0, count: input.length)
+        input.getBytes(&bytes, length: input.length)
+        var hexString = ""
+        for byte in bytes {
+            hexString += String(format:"%02x", UInt8(byte))
+        }
+        return hexString
+    }
+    
+    func hexStringFromData(input: UnsafeMutablePointer<CUnsignedChar>, length: Int) -> String {
+        let hash = NSMutableString()
+        for i in 0..<length {
+            hash.appendFormat("%02x", input[i])
+        }
+        return String(hash)
+    }
 }
+
+public class MD5Hash: baseHash {
+    
+    override var digestLength: Int {
+        return Int(CC_MD5_DIGEST_LENGTH)
+    }
+    
+    override var HMACAlgorithm: CCHmacAlgorithm {
+        return CCHmacAlgorithm(kCCHmacAlgMD5)
+    }
+    
+    @discardableResult
+    override public func normalHash(_ data: UnsafeRawPointer!, _ len: CC_LONG, _ md: UnsafeMutablePointer<UInt8>!) -> UnsafeMutablePointer<UInt8>! {
+        return CC_MD5(data, len, md)
+    }
+    
+    override public func hmacHash(_ algorithm: CCHmacAlgorithm, _ key: UnsafeRawPointer!, _ keyLength: Int, _ data: UnsafeRawPointer!, _ dataLength: Int, _ macOut: UnsafeMutableRawPointer!) {
+        CCHmac(algorithm, key, keyLength, data, dataLength, macOut)
+    }
+}
+
+public class SHA1Hash: baseHash {
+    override var digestLength: Int {
+        return Int(CC_SHA1_DIGEST_LENGTH)
+    }
+    
+    override var HMACAlgorithm: CCHmacAlgorithm {
+        return CCHmacAlgorithm(kCCHmacAlgSHA1)
+    }
+    
+    @discardableResult
+    override public func normalHash(_ data: UnsafeRawPointer!, _ len: CC_LONG, _ md: UnsafeMutablePointer<UInt8>!) -> UnsafeMutablePointer<UInt8>! {
+        return CC_SHA1(data, len, md)
+    }
+    
+    override public func hmacHash(_ algorithm: CCHmacAlgorithm, _ key: UnsafeRawPointer!, _ keyLength: Int, _ data: UnsafeRawPointer!, _ dataLength: Int, _ macOut: UnsafeMutableRawPointer!) {
+        CCHmac(algorithm, key, keyLength, data, dataLength, macOut)
+    }
+}
+
+public class SHA224Hash: baseHash {
+    override var digestLength: Int {
+        return Int(CC_SHA224_DIGEST_LENGTH)
+    }
+    
+    override var HMACAlgorithm: CCHmacAlgorithm {
+        return CCHmacAlgorithm(kCCHmacAlgSHA224)
+    }
+    
+    @discardableResult
+    override public func normalHash(_ data: UnsafeRawPointer!, _ len: CC_LONG, _ md: UnsafeMutablePointer<UInt8>!) -> UnsafeMutablePointer<UInt8>! {
+        return CC_SHA224(data, len, md)
+    }
+    
+    override public func hmacHash(_ algorithm: CCHmacAlgorithm, _ key: UnsafeRawPointer!, _ keyLength: Int, _ data: UnsafeRawPointer!, _ dataLength: Int, _ macOut: UnsafeMutableRawPointer!) {
+        CCHmac(algorithm, key, keyLength, data, dataLength, macOut)
+    }
+}
+
+public class SHA256Hash: baseHash {
+    override var digestLength: Int {
+        return Int(CC_SHA256_DIGEST_LENGTH)
+    }
+    
+    override var HMACAlgorithm: CCHmacAlgorithm {
+        return CCHmacAlgorithm(kCCHmacAlgSHA256)
+    }
+    
+    @discardableResult
+    override public func normalHash(_ data: UnsafeRawPointer!, _ len: CC_LONG, _ md: UnsafeMutablePointer<UInt8>!) -> UnsafeMutablePointer<UInt8>! {
+        return CC_SHA256(data, len, md)
+    }
+    
+    override public func hmacHash(_ algorithm: CCHmacAlgorithm, _ key: UnsafeRawPointer!, _ keyLength: Int, _ data: UnsafeRawPointer!, _ dataLength: Int, _ macOut: UnsafeMutableRawPointer!) {
+        CCHmac(algorithm, key, keyLength, data, dataLength, macOut)
+    }
+}
+
+public class SHA384Hash: baseHash {
+    override var digestLength: Int {
+        return Int(CC_SHA384_DIGEST_LENGTH)
+    }
+    
+    override var HMACAlgorithm: CCHmacAlgorithm {
+        return CCHmacAlgorithm(kCCHmacAlgSHA384)
+    }
+    
+    @discardableResult
+    override public func normalHash(_ data: UnsafeRawPointer!, _ len: CC_LONG, _ md: UnsafeMutablePointer<UInt8>!) -> UnsafeMutablePointer<UInt8>! {
+        return CC_SHA384(data, len, md)
+    }
+    
+    override public func hmacHash(_ algorithm: CCHmacAlgorithm, _ key: UnsafeRawPointer!, _ keyLength: Int, _ data: UnsafeRawPointer!, _ dataLength: Int, _ macOut: UnsafeMutableRawPointer!) {
+        CCHmac(algorithm, key, keyLength, data, dataLength, macOut)
+    }
+}
+
+public class SHA512Hash: baseHash {
+    override var digestLength: Int {
+        return Int(CC_SHA512_DIGEST_LENGTH)
+    }
+    
+    override var HMACAlgorithm: CCHmacAlgorithm {
+        return CCHmacAlgorithm(kCCHmacAlgSHA512)
+    }
+    
+    @discardableResult
+    override public func normalHash(_ data: UnsafeRawPointer!, _ len: CC_LONG, _ md: UnsafeMutablePointer<UInt8>!) -> UnsafeMutablePointer<UInt8>! {
+        return CC_SHA512(data, len, md)
+    }
+    
+    override public func hmacHash(_ algorithm: CCHmacAlgorithm, _ key: UnsafeRawPointer!, _ keyLength: Int, _ data: UnsafeRawPointer!, _ dataLength: Int, _ macOut: UnsafeMutableRawPointer!) {
+        CCHmac(algorithm, key, keyLength, data, dataLength, macOut)
+    }
+}
+
